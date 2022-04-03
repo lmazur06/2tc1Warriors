f11781dd3490abdf7447e7de418fbb629da13b87
maksymtm <maksym.borodulia@tm1.edu.pl>
2022-04-03 23:31:30 +0200

    change drawString text

--- src/com/company/GameField.java
+++ src/com/company/GameField.java
@@ -24,7 +24,7 @@ public class GameField extends JPanel {
 
         for (CharacterClass player : players) {
             g.drawImage(player.getImage(), player.getX(), player.getY(), this);
-            g.drawString("healthPoints: " + player.getHealthPoints(), player.getX(), player.getY() + 70);
+            g.drawString(" " + player.getHealthPoints(), player.getX(), player.getY() + 70);
         }
     }
 
